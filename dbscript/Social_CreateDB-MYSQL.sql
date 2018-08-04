0b1ec32acc2336b75faac3ff4160d5a1ff2fff4e
thachln <thachln@gmail.com>
2018-08-04 07:35:29 +0700

    Load facebook app id from the configuration application.properties into javascript.

---
+++ dbscript/Social_CreateDB-MYSQL.sql
@@ -0,0 +1,7 @@
+Create database social default character set utf8;
+GRANT ALL ON social.* TO social_admin@localhost IDENTIFIED BY 'Social!12345';
+
+-- Useful commands
+-- show databases;
+-- use <database-name>
+-- show tables;
--- src/main/java/com/cts/controller/MainController.java
+++ src/main/java/com/cts/controller/MainController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.social.connect.Connection;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -39,6 +41,7 @@ import com.cts.validator.AppUserValidator;
  */
 @Controller
 @Transactional
+@SessionAttributes("facebookAppId")
 public class MainController {
   
     @Autowired
@@ -53,6 +56,9 @@ public class MainController {
     @Autowired
     private AppUserValidator appUserValidator;
   
+    @Value("${facebook.app.id}")
+    private String facebookAppId;
+
     @InitBinder
     protected void initBinder(WebDataBinder dataBinder) {
   
@@ -73,6 +79,9 @@ public class MainController {
     public String welcomePage(Model model) {
         model.addAttribute("title", "Welcome");
         model.addAttribute("message", "This is welcome page!");
+        
+        model.addAttribute("facebookAppId", facebookAppId);
+
         return "welcomePage";
     }
   
